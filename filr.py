3187701c5237ee9ce5e031d5afab4e3ef260a4df
Lance R. Vick <lance@lrvick.net>
2011-04-05 04:59:43 -0400

    working data dumping

--- filr.py
+++ filr.py
@@ -1,17 +1,49 @@
 import boto
-from kombu import BrokerConnection, Exchange, Queue
+from kombu import BrokerConnection, Exchange, Queue, Consumer
 
 connection = BrokerConnection()
+connection.connect()
+
 channel = connection.channel()
-android_exchange = Exchange("android")
-consumer = Consumer(exchange)
-android_queue = Queue("android", android_exchange)
+exchange = Exchange(
+    name="android", 
+    type="fanout", 
+    channel=channel, 
+    durable=True,
+)
+exchange.declare()
 
-execfile('.private-settings')
+channel = connection.channel()
+queue = Queue(
+    name='filr',
+    exchange=exchange,
+    durable=True,
+    auto_delete=False,
+    channel=channel,
+    routing_key='filr',
+)
+queue.declare();
 
-sdb = boto.connect_sdb(key_id, sec_key)
-domain = sdb.create_domain('android')
-item = domain.new_item('kral_step1')
+def fetch(b,m):
+    print b,m
 
-for key,value in kral_step.items():
-    item[key] = value
+consumer = Consumer(
+    channel=connection.channel(),
+    queues=queue,
+    auto_declare=False,
+    callbacks = [fetch]
+)
+consumer.consume(no_ack=False);
+
+while(True):
+    connection.drain_events()
+    pass
+
+#execfile('.private-settings')
+
+#sdb = boto.connect_sdb(key_id, sec_key)
+#domain = sdb.create_domain('android')
+#item = domain.new_item('kral_step1')
+
+#for key,value in kral_step.items():
+#    item[key] = value
